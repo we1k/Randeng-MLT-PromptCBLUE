71383aa02022932bdffd7f17a320d8444895004e
we1k <819449637@qq.com>
2023-05-15 10:06:05 +0000

    init file

---
+++ src/chatmed_llama_peft/callback.py
@@ -0,0 +1,33 @@
+import os
+import sys
+
+import wandb
+
+import transformers
+from transformers.trainer_callback import TrainerCallback
+from transformers.trainer_utils import PREFIX_CHECKPOINT_DIR
+from transformers import (
+    TrainingArguments,
+    TrainerControl,
+    TrainerState,
+)
+
+class SavePeftModelCallback(TrainerCallback):
+    def on_save(
+        self,
+        args: TrainingArguments,
+        state: TrainerState,
+        control: TrainerControl,
+        **kwargs,
+    ):
+        checkpoint_folder = os.path.join(
+            args.output_dir, f"{PREFIX_CHECKPOINT_DIR}-{state.global_step}"
+        )       
+
+        peft_model_path = os.path.join(checkpoint_folder, "adapter_model")
+        kwargs["model"].save_pretrained(peft_model_path)
+
+        pytorch_model_path = os.path.join(checkpoint_folder, "pytorch_model.bin")
+        if os.path.exists(pytorch_model_path):
+            os.remove(pytorch_model_path)
+        return control
