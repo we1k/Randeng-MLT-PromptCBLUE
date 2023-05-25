867fb9295e620c2b5f1d05d71cc964d20e484544
we1k <819449637@qq.com>
2023-05-25 06:11:16 +0000

    eval randen

--- randen_T5/evaluate.sh
+++ randen_T5/evaluate.sh
@@ -1,11 +1,11 @@
 export WANDB_MODE=disabled
-your_data_path="data/CHIP-STS"  # 填入数据集所在的文件夹路径
+your_data_path="datasets/PromptCBLUE"  # 填入数据集所在的文件夹路径
 your_checkpoint_path="checkpoint/randen"  # 填入用来存储模型的路径
 model_path=IDEA-CCNL/Randeng-T5-784M-MultiTask-Chinese
 
-STEP=50    # 用来评估的模型checkpoint是训练了多少步
+STEP=2000    # 用来评估的模型checkpoint是训练了多少步
 
-CUDA_VISIBLE_DEVICES=1 python randen_T5/main.py \
+CUDA_VISIBLE_DEVICES=0 python randen_T5/main.py \
     --do_predict \
     --checkpoint_path $your_checkpoint_path/checkpoint-$STEP \
     --validation_file $your_data_path/dev.json \
@@ -18,5 +18,5 @@ CUDA_VISIBLE_DEVICES=1 python randen_T5/main.py \
     --overwrite_output_dir \
     --max_source_length 700 \
     --max_target_length 196 \
-    --per_device_eval_batch_size 8 \
+    --per_device_eval_batch_size 16 \
     --predict_with_generate \
